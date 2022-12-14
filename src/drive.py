1a6575b930e30f15d4d5e24ababdf80b9a633061
Berk <berk.yalcin33@gmail.com>
2022-12-14 22:01:37 +0300

    Drive Init, and Robotex CMD-Tool
    Drive class is added and initialization is done. 
    Robotex CMD-Line Tool added and installer updated. Run installer once more.

---
+++ src/drive.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as gpio
+
+
+class Drive:
+    def __init__(self, pins: list, stop_pin: int, drive_pin: int, steer_pin: int) -> None:
+        """Main Drive class. This class is used to control the car's drive system.\n
+        pins: list of pins used for the sensors.\n
+        stop_pin: pin used for the stop button.\n
+        drive_pin: pin used for the drive motor.\n
+        steer_pin: pin used for the steering motor.\n
+        """
+
+        self.gpio = gpio
+        self.sensor_pins = pins
+        self.stop_pin = stop_pin
+        self.drive_pin = drive_pin
+        self.steer_pin = steer_pin
+
+        self.gpio.setmode(self.gpio.Board)
+
+        for pin in self.sensor_pins:
+            self.gpio.setup(pin, self.gpio.IN)
+        self.gpio.setup(self.stop_pin, self.gpio.IN)
+
+        self.gpio.setup(self.drive_pin, self.gpio.OUT)
+        self.gpio.setup(self.steer_pin, self.gpio.OUT)
+
+    def drive(self, speed: int) -> None:
+        """Drive the car.\n
+        speed: speed of the car. Range from 0 to 100.\n
+        """
+        self.gpio.PWM(self.drive_pin, speed)
+
+    def steer(self, angle: int) -> None:
+        """Steer the car.\n
+        angle: angle of the steering wheel. Range from 0 to 100.\n
+        """
+        self.gpio.PWM(self.steer_pin, angle)
+
+    def update(self, speed: int, angle: int) -> None:
+        """Update the drive system.\n
+        CONTINUE HERE AND CREATE A LOG FILE FOR THE DRIVE SYSTEM.\n
+        CREATE ANOTHER LOG FILE FOR THE SENSOR SYSTEM.\n    
+        """
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from drive import Drive
+
+if __name__ == "__main__":
+    # Change pin numbers before running
+    drive = Drive([1, 2, 3], 4, 5, 6)
+    print("Pins are set up correctly...")
