eb9badcf347ba0365cf7b500ca086d4d4d65afb6
Berk <berk.yalcin33@gmail.com>
2023-02-22 13:35:21 +0300

    Changes to drive and DriveTest.py

--- Tests/DriveTest.py
+++ Tests/DriveTest.py
@@ -1,18 +1,20 @@
 # Include the library files
 import RPi.GPIO as GPIO
 from time import sleep
+from src.drive import Drive
 
+d = Drive()
 # Include the motor control pins
 #TODO: Pins overlap with prexistting sensor circuitery but since the sensor circuits dont exist right now it fine. Switch sensor pins or motor pins
 #TODO: make the ENA Pins the same pin so that a single pin can be a switch
 #The speed control idea is canceled
 
-#pins for left motor,
+#pins for motor 1(left),
 ENA = 17  
 IN1 = 27 #Foward pin for the motor
 IN2 = 22
 
-#pins for motor 2, currently doesnt work
+#pins for motor 2(right), currently doesnt work
 ENA2=37
 IN3=35
 IN4=33
@@ -35,8 +37,6 @@ def forward(): #foward is correct
     GPIO.output(IN3,GPIO.HIGH)
     GPIO.output(IN4,GPIO.LOW)
 
-
-
 def backward():
     GPIO.output(ENA,GPIO.HIGH)
     GPIO.output(IN1,GPIO.LOW)
@@ -56,10 +56,17 @@ def stop():
 
 try:
     while True:
-        forward()
-        sleep(1)
-        backward()
-        sleep(1)
+        d.forward()
+        sleep(0.2)
+        d.backward()
+        sleep(0.2)
+        d.right()
+        sleep(0.2)
+        d.left()
+        sleep(0.2)
+        stop()
+        quit()
 except:
 
     stop()
+    quit()
